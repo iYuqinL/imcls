90692a0afa5a24108602473edff27c58e5bbe7de
Yujam Leung <yusnowsliang@gmail.com>
2019-10-16 22:48:43 +0800

    data_process/operations.py: add ExifTranspose

--- data_process/operations.py
+++ data_process/operations.py
@@ -27,6 +27,46 @@ import math
 import warnings
 
 
+class ExifTranspose(object):
+    def __init__(self):
+        super(ExifTranspose, self).__init__()
+
+    def __call__(self, img):
+        if not img:
+            return img
+        exif_orientation_tag = 274
+        # Check for EXIF data (only present on some files)
+        if hasattr(img, "_getexif") and isinstance(img._getexif(), dict) and exif_orientation_tag in img._getexif():
+            exif_data = img._getexif()
+            orientation = exif_data[exif_orientation_tag]
+            # Handle EXIF Orientation
+            if orientation == 1:
+                # Normal image - nothing to do!
+                pass
+            elif orientation == 2:
+                # Mirrored left to right
+                img = img.transpose(Image.FLIP_LEFT_RIGHT)
+            elif orientation == 3:
+                # Rotated 180 degrees
+                img = img.rotate(180)
+            elif orientation == 4:
+                # Mirrored top to bottom
+                img = img.rotate(180).transpose(Image.FLIP_LEFT_RIGHT)
+            elif orientation == 5:
+                # Mirrored along top-left diagonal
+                img = img.rotate(-90, expand=True).transpose(Image.FLIP_LEFT_RIGHT)
+            elif orientation == 6:
+                # Rotated 90 degrees
+                img = img.rotate(-90, expand=True)
+            elif orientation == 7:
+                # Mirrored along top-right diagonal
+                img = img.rotate(90, expand=True).transpose(Image.FLIP_LEFT_RIGHT)
+            elif orientation == 8:
+                # Rotated 270 degrees
+                img = img.rotate(90, expand=True)
+        return img
+
+
 class HistogramEqualisation(object):
     """
     The class :class:`HistogramEqualisation` is used to perform histogram
